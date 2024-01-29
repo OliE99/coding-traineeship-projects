434be3c2ea89a0f3fbc59a9e1a86cfb66ec36c40
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-01-29 11:30:39 +0000

    Finished project

--- 054.Adopt-A-Pet/adopt-a-pet/src/App.js
+++ 054.Adopt-A-Pet/adopt-a-pet/src/App.js
@@ -6,8 +6,15 @@ import Root from './components/root';
 
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
-// create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={ <Root/> }>
+    <Route index element={ <HomePage/> }/>
+    <Route path=":type" element={ <HomePage/> }/>
+    <Route path=":type/:id" element={ <PetDetailsPage/> }/>
+    <Route path="search" element={ <SearchPage/> }/>
+    <Route path="pet-details-not-found" element={ <PetDetailsNotFound/> }/>
+  </Route>
+))
 
 function App() {
   return (
