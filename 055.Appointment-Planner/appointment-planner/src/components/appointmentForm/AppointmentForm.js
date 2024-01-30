a411bcd30440c71dcd4c691a594fc5b2116969d5
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-01-30 14:53:35 +0000

    Finished project

--- 055.Appointment-Planner/appointment-planner/src/components/appointmentForm/AppointmentForm.js
+++ 055.Appointment-Planner/appointment-planner/src/components/appointmentForm/AppointmentForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {ContactPicker} from '../contactPicker/ContactPicker';
 
 const getTodayString = () => {
   const [month, day, year] = new Date()
@@ -22,6 +22,15 @@ export const AppointmentForm = ({
 }) => {
 
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="title">Title:</label>
+      <input type='text' id='title' name='appointment-title' value={title} required onChange={(e) => {setTitle(e.target.value)}} />
+      <label htmlFor='date'>Date:</label>
+      <input type='date' id='date' name='appointment-date' value={date} required onChange={(e) => {setDate(e.target.value)}} min={getTodayString()} />
+      <label htmlFor='time'>Time:</label>
+      <input type='time' id='time' name='appointment-time' value={time} required onChange={(e) => {setTime(e.target.value)}} />
+      <ContactPicker contacts={contacts} name='appointment-contact' value={contact} onChange={(e) => {setContact(e.target.value)}} />
+      <button type='submit'>Save</button>
+    </form>
   );
 };
--- 055.Appointment-Planner/appointment-planner/src/components/tile/Tile.js
+++ 055.Appointment-Planner/appointment-planner/src/components/tile/Tile.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({name, description}) => {
   return (
     <div className="tile-container">
-      
+      <p className="tile-title">{name}</p>
+      {Object.values(description).map((value, index) => {
+        return (<p className="tile" key="{`attb-${index}">{value}</p>)
+      })}
     </div>
   );
 };
