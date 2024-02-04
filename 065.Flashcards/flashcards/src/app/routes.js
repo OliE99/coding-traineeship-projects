f561f78b497141bd2347754dcb80662adf7f5c9e
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-02-04 14:18:19 +0000

    Added topicSlice and exported to topics.js

--- 065.Flashcards/flashcards/src/app/routes.js
+++ 065.Flashcards/flashcards/src/app/routes.js
@@ -8,4 +8,3 @@ const ROUTES = {
   };
   
   export default ROUTES;
-  
--- 065.Flashcards/flashcards/src/features/topics/Topics.js
+++ 065.Flashcards/flashcards/src/features/topics/Topics.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ROUTES from "../../app/routes";
-// import selector
+import { selectTopics } from './topicsSlice';
+import { useSelector } from 'react-redux';
 
 export default function Topics() {
   const topics = {}; // replace this with a call to your selector to select all the topics in state
