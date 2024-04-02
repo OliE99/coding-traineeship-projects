9472970644ef64c86b587fe30702e241039390d9
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-04-02 11:58:27 +0100

    Create initials.py

---
+++ 104.Block-Letters/initials.py
@@ -0,0 +1,30 @@
+# Oliver Norman Elliott
+# I'm great at impressions and accents!
+
+print("ASCII art:")
+
+print(" OOO   N     N  EEEEE")
+print("O   O  NN    N  E")
+print("O   O  N N   N  E")
+print("O   O  N   N N  EEE")
+print("O   O  N     N  E")
+print("O   O  N     N  E")
+print(" OOO   N     N  EEEEE")
+
+
+print("      *     *                    ")
+print("                      *          ")
+print("*         *                 *    ")
+print("     *           *     *         ")
+print("           HHHHHHH        *      ")
+print(" *         HHHHHHH               ")
+print("     *   HHHHHHHHHHH    *     *  ")
+print("         *   @ @   *             ")
+print("        *     ^     *            ")
+print("         SSS * * S               ")
+print("         * SSSSSSSS *            ")
+print("       *      O   SS  *          ")
+print("      *       O    S   *         ")
+print("      *       O        *         ")
+print("       *              *          ")
+print("           *   *   *             ")
